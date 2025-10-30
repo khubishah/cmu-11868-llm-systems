29c9ee15b01fd16d92e885d86bb83f0cf2fb5746
Khubi Shah <shah.khubi@gmail.com>
2025-10-29 21:06:55 -0700

    removed other stuff from hw5 branch

--- llmsys_f25_hw4-main/README.md
+++
@@ -1,3 +0,0 @@
-# llmsys_f25_hw4
-
-Public repository for Assignment 3 of 11-868 LLM Systems.
--- llmsys_f25_hw4-main/compile_cuda.sh
+++
@@ -1,2 +0,0 @@
-mkdir -p minitorch/cuda_kernels
-nvcc -o minitorch/cuda_kernels/combine.so --shared src/combine.cu -Xcompiler -fPIC
